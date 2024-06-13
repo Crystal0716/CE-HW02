b33b9673f3094b57f8e534d5ac09fab9ee16cac1
若水 <168269104+Crystal0716@users.noreply.github.com>
2024-06-13 21:01:22 +0800

    Add files via upload

---
+++ HW02.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	int i ;
+	i =64122038;
+	printf("My ID is E");
+	cout<<i;
+	printf("\n");
+	printf("Even number");
+	return(0);
+}
